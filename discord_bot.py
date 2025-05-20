86053f9653ced91e721cbe374fa6fea02f09cbc2
Alecoow <alecoow@gmail.com>
2025-05-19 17:52:44 -0700

    Replace instances of single quotes with double for consistency

--- discord_bot.py
+++ discord_bot.py
@@ -32,27 +32,27 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    elif message.content.startswith('$msg'):
+    elif message.content.startswith("$msg"):
         if response_lock.locked():
             await message.channel.send("Server busy! Please wait")
             return
         async with response_lock:
             # we don't need to send $msg to the LLM
-            msg = message.content.lstrip('$msg')
+            msg = message.content.lstrip("$msg")
             convo.append_message(msg)
             payload = convo.build_payload()
             await message.channel.send("Generating...")
             loop = asyncio.get_running_loop()
             response = await loop.run_in_executor(None, message_llm, payload)
-            convo.append_message(response, 'assistant')
+            convo.append_message(response, "assistant")
             await message.channel.send(response)
-    elif message.content.startswith('$clear'):
+    elif message.content.startswith("$clear"):
         convo.delete_history()
-        await message.channel.send('Memory cleared')
+        await message.channel.send("Memory cleared")
         
 def query_models():
     try:
-        response = requests.get(f'{LLM_ENDPOINT}/models')
+        response = requests.get(f"{LLM_ENDPOINT}/models")
         response.raise_for_status()
         print(response.json())
     except requests.exceptions.RequestException as e:
@@ -69,7 +69,7 @@ def message_llm(message):
         # parse response to trim out unneccessary information + limit response to 2000 characters to comply with discord limits
         json_response = response.json()
         parsed_response = json_response["choices"][0]["message"]["content"]
-        parsed_response = (parsed_response[:1976] + '\n\n [RESPONSE TRUNCATED]') if len(parsed_response) > 1976 else parsed_response
+        parsed_response = (parsed_response[:1976] + "\n\n [RESPONSE TRUNCATED]") if len(parsed_response) > 1976 else parsed_response
         return parsed_response
     
     except requests.exceptions.RequestException as e:
